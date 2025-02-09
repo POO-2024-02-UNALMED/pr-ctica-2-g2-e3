b09967b51a4f084d7a657a19af105aa4e079f2c4
Samuel <samuelrb34@gmail.com>
2025-02-09 16:58:02 -0500

    Create CategoriaHabitacion.py

---
+++ src/gestorAplicacion/administracion/CategoriaHabitacion.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+class CategoriaHabitacion(Enum):
+    CAMILLA = 50000
+    INDIVIDUAL = 150000
+    DOBLE = 320000
+    OBSERVACION = 500000
+    UCI = 1300000
+    UCC = 1500000
+    
+    def get_valor(self):
+        return self.value
